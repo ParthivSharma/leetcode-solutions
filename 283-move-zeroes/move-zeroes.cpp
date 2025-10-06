560486ea120a42511f082c7336b7d4e5c8dee493
PARTHIV SHARMA <146014388+ParthivSharma@users.noreply.github.com>
2025-10-06 21:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.21%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[l] , nums[i]);
+                l++;
+            }
+        }
+        
+    }
+};
